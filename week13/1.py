f28a21876767549fd472711674a32462d6eddf6f
Beisenbek Baisakov <bbaisakov@dar.kz>
2023-04-22 14:41:50 +0600

    upd

---
+++ week13/1.py
@@ -0,0 +1,17 @@
+import psycopg2
+
+conn = psycopg2.connect("dbname=suppliers2 user=bsnbk password=password2")
+
+# create a cursor
+cur = conn.cursor()
+
+# execute a statement
+print('PostgreSQL database version:')
+cur.execute('SELECT version()')
+
+# display the PostgreSQL database server version
+db_version = cur.fetchone()
+print(db_version)
+
+# close the communication with the PostgreSQL
+cur.close()
